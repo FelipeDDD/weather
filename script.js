3a3309ed58a2ecfa69363e5accf68a961d7df195
FelipeDDD <felipedias253@hotmail.com>
2024-06-21 02:34:09 +0200

    Finished App

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=Bonn&format=json&u=c';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '86deea953amsh9188b589ad5f3b8p176901jsn7774713e42a8',
-		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
-	}
-};
+function getWeather(city) {
+     const url = `http://localhost:3000/locations/${city}`
+//   const url = `https://yahoo-weather5.p.rapidapi.com/weather?location=${city}&format=json&u=c`;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-key": "f6aa5f2322mshfe15ec228adcb0ap16dc73jsn7ceb62b13bef",
+//       "x-rapidapi-host": "yahoo-weather5.p.rapidapi.com",
+//     },
+//   };
 
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => weatherFunction(data))
+    .catch((err) => console.error(err));
+}
 // const weather = async() => {
 // try {
 // 	const response = await fetch(url, options);
@@ -16,16 +23,36 @@ const options = {
 // 	console.error(error);
 // }}
 // weather()
-const weatherDiv = document.getElementById("weatherDiv")
+const weatherDiv = document.getElementById("weatherDiv");
+const weatherCondition = document.getElementById("weatherCondition");
+const city = document.getElementById("city");
+const searchInput = document.getElementById("searchInput");
 
-fetch(url, options)
-    .then(response => response.json())
-    .then(data => weatherFunction(data))
-    .catch(err => console.error(err))
-    
 function weatherFunction(data) {
-    currentTemp = `<h2>${data.current_observation.condition.temperature}</h2>`
-    
+  let weatherC = `${data.current_observation.condition.text}`;
+  let currentTemp = `<h5>Temperatura: ${data.current_observation.condition.temperature}ºC</h5>`;
+  let minTemp = `<h5>Temperatura mínima: ${data.forecasts[0].low}ºC</h5>`;
+  let maxTemp = `<h5>Temperatura máxima: ${data.forecasts[0].high}ºC</h5>`;
+  let cityInput = `${ data.location.city}`
+ 
+  city.textContent = `${cityInput}`
+  weatherCondition.textContent = `${weatherC}`;
+  weatherDiv.innerHTML = `${currentTemp}${minTemp}${maxTemp}`;
+}
 
-    weatherDiv.innerHTML = `${#}`
+function searchClick() {
+  const searchCity = document.getElementById("searchInput").value;
+  if (searchCity != "") {
+    getWeather(searchCity);
+  } else {
+    alert("perdeu");
+  }
+}
+
+searchButton.addEventListener("click", searchClick);
+
+function check(x) {
+  Array.isArray(x)
+    ? console.log("É um array")
+    : console.log("Não é um array, é um " + typeof x);
 }
